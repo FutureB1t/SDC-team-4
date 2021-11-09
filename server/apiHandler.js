13c39282876cdda1140e99049e18f5f678815f02
Curtis Castro <curtisncastro@gmail.com>
2021-11-09 03:51:40 -0800

    Improve FetchQandA endpoint performance speed by 1000% through SQL optimization

--- server/apiHandler.js
+++ server/apiHandler.js
@@ -5,21 +5,6 @@ const moment = require('moment');
 const app = require('axios');
 const db = require('../database');
 
-const nestQueryObj = (rowKey, query) => (
-  `coalesce(
-    ( SELECT to_json(jsonb_object_agg(${rowKey}, row_to_json(X)))
-      FROM (${query}) X
-    ), '{}'
-  )`
-);
-
-const nestQueryArr = (query) => (
-  `coalesce(
-    ( SELECT to_json(array_agg(x))
-      FROM (${query}) x
-    ), '[]'
-  )`
-);
 
 const fetchProduct = (id, cb) => {
   const queryStr = `SELECT * FROM product WHERE id = ${id}`;
@@ -36,31 +21,6 @@ const queries = {
   qs: 'SELECT * FROM questions WHERE product_id = ',
   as: 'SELECT * FROM answers WHERE question_id = ',
   pics: 'SELECT * FROM photos WHERE answer_id = ',
-  queryQs:
-    `SELECT
-    q.question_id,
-    q.body AS question_body,
-    q.date_written AS question_date,
-    q.asker_name,
-    q.helpful AS question_helpfulness,
-    q.reported,
-    ${nestQueryObj('id',
-      `SELECT
-        a.answer_id AS id,
-        a.body,
-        a.date,
-        a.answerer_name,
-        a.helpfulness,
-        ${nestQueryArr(
-        `SELECT url
-          FROM photos p
-          WHERE p.answer_id = a.answer_id`,
-        )} AS photos
-      FROM answers a
-      WHERE a.question_id = q.question_id`)
-    } AS answers
-  FROM questions q
-  WHERE product_id = `,
 };
 
 const fetchQs = async (product_id, cb) => {
@@ -95,15 +55,34 @@ const fetchPics = async (answer_id, cb) => {
   });
 };
 
+
 const fetchQandA = async (product_id, cb) => {
-  const query = `Select *
+  const queryStr = `Select
+      questions.question_id,
+      questions.product_id,
+      questions.body as question_body,
+      questions.date_written,
+      questions.asker_name,
+      questions.asker_email,
+      questions.reported as question_reported,
+      questions.helpful,
+      answers.answer_id,
+      answers.date as answer_date,
+      answers.answerer_name,
+      answers.answerer_email,
+      answers.body as answer_body,
+      answers.reported as answer_reported,
+      answers.helpfulness,
+      photos.photo_id,
+      photos.url
     from questions
     left join answers on questions.question_id = answers.question_id
     left join photos on answers.answer_id = photos.answer_id
     where questions.product_id = ${product_id}
+    -- and answers.question_id = questions.question_id
+    -- and photos.answer_id = answers.answer_id
     order by questions.question_id;`;
-  await db.query(query, (err, data) => {
-    let result = {};
+  await db.query(queryStr, (err, data) => {
     const dbRows = data.rows;
     let container = {
       questions: {},
@@ -111,33 +90,70 @@ const fetchQandA = async (product_id, cb) => {
       photos: {},
     };
     dbRows.forEach((r) => {
-      if (!container.questions[r.question_id] && r.question_id) {
-        container.questions[r.question_id] = r.body;
-      } else if (!container.answers[r.answer_id] && r.answer_id) {
-        container.answers[r.answer_id] = r.answer_id;
-      } else if (!container.photos[r.photo_id] && r.photo_id) {
-        container.photos[r.photo_id] = r.photo_id;
-      } else {
-        console.log(r);
+      const {
+        question_id,
+        question_body,
+        date_written,
+        asker_name,
+        helpful,
+        question_reported,
+        answer_id,
+        answer_date,
+        answer_body,
+        answerer_name,
+        helpfulness,
+        photo_id,
+        url,
+      } = r;
+      const models = {
+        answerModel: {
+          id: answer_id,
+          body: answer_body,
+          date: answer_date,
+          photos: [],
+          helpfulness,
+          answerer_name,
+          question_id,
+        },
+        questionModel: {
+          question_id,
+          question_body,
+          question_date: date_written,
+          asker_name,
+          question_helpfulness: helpful,
+          reported: question_reported,
+          answers: {},
+        },
+        photoModel: {
+          url,
+          answer_id,
+          photo_id,
+        },
+      };
+      if (question_body && answer_body && url) {
+        container.questions[question_id] = models.questionModel;
+        container.answers[answer_id] = models.answerModel;
+        container.photos[answer_id] = models.photoModel;
       }
-      // console.log(r);
-      // const res = {};
-      // res.question_id = r.question_id;
-      // res.product_id = r.product_id;
-      // res.body = r.body;
-      // res.date_written = r.date_written;
-      // res.asker_name = r.asker_name;
-      // res.asker_email = r.asker_email;
-      // res.reported = r.reported;
-      // res.helpful = r.helpful;
-      // res.answers = [];
-      // container.questions[r.question_id] = res;
+      if (question_body && answer_body && !url) {
+        container.questions[question_id] = models.questionModel;
+        container.answers[answer_id] = models.answerModel;
+      }
+      if (question_body && !answer_body && !url) {
+        container.questions[question_id] = models.questionModel;
+      }
+    });
+    Object.keys(container.photos).forEach((k) => {
+      container.answers[k].photos.push({ url: `${container.photos[k].url}` });
+    });
+    Object.values(container.answers).forEach((v) => {
+      container.questions[v.question_id].answers[v.id] = v;
     });
     if (err) {
       console.log(err);
     } else {
-      console.log('SENDING DATA', container);
-      cb(container);
+      console.log('SENDING DATA');
+      cb(container.questions);
     }
   });
 };
