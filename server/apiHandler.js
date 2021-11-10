1e3f84e96ed2fcca071a302bb2a74be884335c5c
Curtis Castro <curtisncastro@gmail.com>
2021-11-09 21:17:02 -0800

    Implement AddQuestion endpoint

--- server/apiHandler.js
+++ server/apiHandler.js
@@ -18,13 +18,13 @@ const fetchProduct = (id, cb) => {
 };
 
 const queries = {
-  qs: 'SELECT * FROM questions WHERE product_id = ',
   as: 'SELECT * FROM answers WHERE question_id = ',
   pics: 'SELECT * FROM photos WHERE answer_id = ',
 };
 
 const fetchQs = async (product_id, cb) => {
-  const queryStr = queries.qs + product_id.toString();
+  const queryStr =
+    'SELECT * FROM questions WHERE product_id = ' + product_id.toString();
   await db.query(queryStr, (err, data) => {
     if (err) {
       console.log('ERROR: ', err);
@@ -54,7 +54,6 @@ const fetchPics = async (answer_id, cb) => {
     }
   });
 };
-
 
 const fetchQandA = async (product_id, cb) => {
   const queryStr = `Select
@@ -89,6 +88,9 @@ const fetchQandA = async (product_id, cb) => {
       answers: {},
       photos: {},
     };
+    let {
+      questions, answers, photos,
+    } = container;
     dbRows.forEach((r) => {
       const {
         question_id,
@@ -97,6 +99,93 @@ const fetchQandA = async (product_id, cb) => {
         asker_name,
         helpful,
         question_reported,
+        answer_id,
+        answer_date,
+        answer_body,
+        answerer_name,
+        helpfulness,
+        photo_id,
+        url,
+      } = r;
+      const models = {
+        questionModel: {
+          question_id,
+          question_body,
+          question_date: date_written,
+          asker_name,
+          question_helpfulness: helpful,
+          reported: question_reported,
+          answers: {},
+        },
+        answerModel: {
+          id: answer_id,
+          body: answer_body,
+          date: answer_date,
+          photos: [],
+          helpfulness,
+          answerer_name,
+          question_id,
+        },
+        photoModel: {
+          url,
+          answer_id,
+          photo_id,
+        },
+      };
+      if (question_body && answer_body && url) {
+        questions[question_id] = models.questionModel;
+        answers[answer_id] = models.answerModel;
+        photos[answer_id] = models.photoModel;
+      }
+      if (question_body && answer_body && !url) {
+        questions[question_id] = models.questionModel;
+        answers[answer_id] = models.answerModel;
+      }
+      if (question_body && !answer_body && !url) {
+        questions[question_id] = models.questionModel;
+      }
+    });
+    Object.keys(photos).forEach((k) => {
+      answers[k].photos.push({ url: `${photos[k].url}` });
+    });
+    Object.values(answers).forEach((v) => {
+      questions[v.question_id].answers[v.id] = v;
+    });
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('SENDING DATA');
+      cb(questions);
+    }
+  });
+};
+
+const fetchAsForQ = async (question_id, cb) => {
+  const queryStr = `Select
+      answers.answer_id,
+      answers.date as answer_date,
+      answers.answerer_name,
+      answers.answerer_email,
+      answers.body as answer_body,
+      answers.reported as answer_reported,
+      answers.helpfulness,
+      photos.photo_id,
+      photos.url
+    from answers
+    left join photos on answers.answer_id = photos.answer_id
+    where answers.question_id = ${question_id}
+    order by answers.question_id;`;
+  await db.query(queryStr, (err, data) => {
+    const dbRows = data.rows;
+    let container = {
+      answers: {},
+      photos: {},
+    };
+    let {
+      answers, photos,
+    } = container;
+    dbRows.forEach((r) => {
+      const {
         answer_id,
         answer_date,
         answer_body,
@@ -115,78 +204,32 @@ const fetchQandA = async (product_id, cb) => {
           answerer_name,
           question_id,
         },
-        questionModel: {
-          question_id,
-          question_body,
-          question_date: date_written,
-          asker_name,
-          question_helpfulness: helpful,
-          reported: question_reported,
-          answers: {},
-        },
         photoModel: {
           url,
           answer_id,
           photo_id,
         },
       };
-      if (question_body && answer_body && url) {
-        container.questions[question_id] = models.questionModel;
-        container.answers[answer_id] = models.answerModel;
-        container.photos[answer_id] = models.photoModel;
+      if (answer_body && url) {
+        answers[answer_id] = models.answerModel;
+        photos[answer_id] = models.photoModel;
       }
-      if (question_body && answer_body && !url) {
-        container.questions[question_id] = models.questionModel;
-        container.answers[answer_id] = models.answerModel;
-      }
-      if (question_body && !answer_body && !url) {
-        container.questions[question_id] = models.questionModel;
+      if (answer_body && !url) {
+        answers[answer_id] = models.answerModel;
       }
     });
-    Object.keys(container.photos).forEach((k) => {
-      container.answers[k].photos.push({ url: `${container.photos[k].url}` });
-    });
-    Object.values(container.answers).forEach((v) => {
-      container.questions[v.question_id].answers[v.id] = v;
+    Object.keys(photos).forEach((k) => {
+      answers[k].photos.push({ url: `${photos[k].url}` });
     });
     if (err) {
       console.log(err);
     } else {
-      console.log('SENDING DATA');
-      cb(container.questions);
+      console.log('SENDING DATA', answers);
+      cb(answers);
     }
   });
 };
 
-const fetchAsForQ = async (question_id, cb) => {
-  const queryStr = `SELECT
-      a.answer_id,
-      a.body,
-      a.date,
-      a.answerer_name,
-      a.helpfulness,
-      ${nestQueryArr(`SELECT
-        p.photo_id AS id,
-        p.url
-        FROM photos p
-        // eslint-disable-next-line comma-dangle
-        WHERE p.answer_id = a.answer_id`)}
-        AS photos
-    FROM answers a
-    WHERE a.question_id = ${question_id}`;
-  await db.query(queryStr, (err, res) => {
-    if (err) {
-      console.log(err);
-    } else {
-      cb({
-        question: question_id,
-        page: 1,
-        count: res.rows.length,
-        results: res.rows,
-      });
-    }
-  });
-};
 const addQuestion = async (content, cb) => {
   let {
     product_id,
@@ -201,8 +244,9 @@ const addQuestion = async (content, cb) => {
   console.log('QUERY: ', queryStr);
   await db.query(queryStr, (err) => {
     if (err) {
-      console.error(err);
+      console.log(err);
     } else {
+      console.log('Adding Answer');
       cb();
     }
   });
@@ -216,12 +260,12 @@ const addAnswer = async (question_id, content, cb) => {
     ON CONFLICT ON CONSTRAINT answers_pkey
     DO NOTHING;
   `;
-  console.time('add answer query time');
+  // console.time('add answer query time');
   await db.query(queryStr, (err) => {
     if (err) {
       console.error(err);
     } else {
-      console.timeEnd('add answer query time');
+      // console.timeEnd('add answer query time');
       cb();
     }
   });
